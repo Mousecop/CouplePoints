3cb2b779d936dd47116571666e4accb93ed9e89d
Mousecop <joaskew13@gmail.com>
2018-06-18 19:50:10 -0400

    Creates Basic component

---
+++ src/components/CreateGame/createGame.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class CreateGame extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Create a game!</Text>
+			</View>
+		);
+	}
+}
---
+++ src/components/JoinGame/joinGame.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class JoinGame extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Join a game!</Text>
+			</View>
+		);
+	}
+}
