ee0eee0a332a863bb34318e082ff0df60647e7e4
Mousecop <joaskew13@gmail.com>
2018-06-18 14:19:40 -0400

    Inits Firebase and import router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import * as firebase from 'firebase';
+import AppNavigator from './navigation/AppNavigator';
 
+const firebaseConfig = {
+	apiKey: 'AIzaSyCP8JDSIlp26GvTIkJe84FmFNitp1-p864',
+	authDomain: 'couplepoints-2bdbe.firebaseapp.com',
+	databaseURL: 'https://couplepoints-2bdbe.firebaseio.com',
+	projectId: 'couplepoints-2bdbe',
+	storageBucket: '',
+	messagingSenderId: '453625238827'
+};
+
+firebase.initializeApp(firebaseConfig);
 export default class App extends React.Component {
 	render() {
-		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<Text>Changes you make will automatically reload.</Text>
-				<Text>Shake your phone to open the developer menu.</Text>
-			</View>
-		);
+		return <AppNavigator />;
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
-});
