e936a2334a26c35dc99244ff99d4d74b1cfe2adf
Mousecop <joaskew13@gmail.com>
2018-06-18 19:50:28 -0400

    Adds nav styles, renders inital screen

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
-import {
-	View,
-	Text,
-	TextInput,
-	Button,
-	StyleSheet,
-	Dimensions,
-	AsyncStorage
-} from 'react-native';
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
+import { InitialScreen } from '../shared/initalScreen';
 
 export interface Props {
-	navigation: any;
+	navigation?: any;
 }
 
 export class Home extends Component<Props> {
+	static navigationOptions = {
+		title: 'Home',
+		headerStyle: {
+			backgroundColor: '#81c6ff'
+		},
+		headerTitleStyle: {
+			color: '#FFF',
+			fontSize: 20
+		}
+	};
+
 	constructor(props: Props) {
 		super(props);
 	}
 	render() {
 		return (
-			<Button
-				title="Signout"
-				onPress={async () => {
-					await AsyncStorage.removeItem('userToken');
-					firebase.auth().signOut();
-					this.props.navigation.navigate('Auth');
-				}}
-			/>
+			<InitialScreen navigation={this.props.navigation} />
+			// <Button
+			// 	title="Signout"
+			// 	onPress={async () => {
+			// 		await AsyncStorage.removeItem('userToken');
+			// 		firebase.auth().signOut();
+			// 		this.props.navigation.navigate('Auth');
+			// 	}}
+			// />
 		);
 	}
 }
