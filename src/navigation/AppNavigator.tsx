9ef3ab0240d5d0198e5aad0c5354c071e8f57b84
Mousecop <joaskew13@gmail.com>
2018-06-20 13:08:28 -0400

    Adds Homeloading component

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,5 +1,9 @@
 // import React, { Component } from 'react';
-import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
+import {
+	createStackNavigator,
+	createSwitchNavigator,
+	createBottomTabNavigator
+} from 'react-navigation';
 /* Route Component imports */
 import { Signup } from '../components/Signup/signup';
 import { Login } from '../components/Login/login';
@@ -8,20 +12,29 @@ import { AuthLoading } from '../AuthLoading/AuthLoadingScreen';
 import { JoinGame } from '../components/JoinGame/joinGame';
 import { NoGameFound } from '../components/shared/noGameFound';
 import { CreateGame } from '../components/CreateGame/createGame';
+import { HomeLoading } from '../AuthLoading/HomeLoadingScreen';
 
 const authStack = createStackNavigator({ Login: Login, Signup: Signup });
-const appStack = createStackNavigator({
-	Home: Home,
-	initalScreen: NoGameFound,
+const homeStack = createStackNavigator({
+	Home: Home
+});
+const noGameFoundStack = createStackNavigator({
+	noGameFound: NoGameFound,
 	Join: JoinGame,
 	Create: CreateGame
+});
+
+const appTab = createBottomTabNavigator({
+	Home: homeStack
 });
 
 export default createSwitchNavigator(
 	{
 		AuthLoadingScreen: AuthLoading,
-		App: appStack,
-		Auth: authStack
+		App: HomeLoading,
+		Home: appTab,
+		Auth: authStack,
+		NoGameFound: noGameFoundStack
 	},
 	{
 		initialRouteName: 'AuthLoadingScreen'
