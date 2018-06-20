cb3598e36f04d52e5e743f8dff218de477982e05
Mousecop <joaskew13@gmail.com>
2018-06-20 13:07:18 -0400

    Creates component from Home routing logic

---
+++ src/AuthLoading/HomeLoadingScreen.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { ActivityIndicator, AsyncStorage, StatusBar, View } from 'react-native';
+import * as firebase from 'firebase';
+
+export interface Props {
+	navigation: any;
+}
+
+export class HomeLoading extends Component<Props> {
+	constructor(props: Props) {
+		super(props);
+		this._bootstrapAsync();
+	}
+
+	_bootstrapAsync = async () => {
+		const userToken = await AsyncStorage.getItem('userToken').then(
+			result => result
+		);
+		firebase
+			.database()
+			.ref('users/' + userToken)
+			.once('value')
+			.then(snapshot => {
+				let user = snapshot.val();
+				if (!user.game) {
+					this.props.navigation.navigate('NoGameFound');
+				} else {
+					this.props.navigation.navigate('Home');
+				}
+			});
+	};
+
+	render() {
+		return (
+			<View>
+				<ActivityIndicator />
+				<StatusBar barStyle="default" />
+			</View>
+		);
+	}
+}
