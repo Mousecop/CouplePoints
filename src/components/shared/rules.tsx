03b3a0f9f43223a7b2ef3ffa71deae556648aa79
Mousecop <joaskew13@gmail.com>
2018-06-21 09:15:22 -0400

    Basic component structure

---
+++ src/components/shared/rules.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class Rules extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Rules here!</Text>
+			</View>
+		);
+	}
+}
