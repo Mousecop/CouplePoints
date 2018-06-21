6c85680d97d2f047c2765821e6b53c38a0c8efa2
Mousecop <joaskew13@gmail.com>
2018-06-21 09:15:13 -0400

    WIP Builds out part of UI for displaying current game

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,48 +1,139 @@
 import React, { Component } from 'react';
-// import * as firebase from 'firebase';
-// import { NoGameFound } from '../shared/noGameFound';
-import { Text, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo';
+import * as firebase from 'firebase';
+import {
+	Text,
+	StyleSheet,
+	AsyncStorage,
+	View,
+	ActivityIndicator,
+	Image
+} from 'react-native';
 
 export interface Props {
 	navigation?: any;
 }
 
-export class Home extends Component<Props> {
+export interface State {
+	user: any;
+	game: any;
+	players: any;
+}
+
+export class Home extends Component<Props, State> {
 	static navigationOptions = {
-		title: 'Home',
+		title: 'Couple Points',
 		headerStyle: {
-			backgroundColor: '#FF4E50',
-			borderBottomColor: 'transparent'
+			backgroundColor: '#FFF',
+			paddingBottom: 10
 		},
 		headerTitleStyle: {
-			color: '#FFF',
-			fontSize: 20
+			color: '#FF4E50',
+			fontSize: 26
 		}
 	};
 
 	constructor(props: Props) {
 		super(props);
+
+		this.state = {
+			user: undefined,
+			game: undefined,
+			players: []
+		};
+
+		this.initalizeGame();
 	}
-	render() {
-		return (
-			<LinearGradient colors={['#FF4E50', '#F9D423']} style={styles.container}>
-				<Text>Hello Orld</Text>
-			</LinearGradient>
-			// <Button
-			// 	title="Signout"
-			// 	onPress={async () => {
-			// 		await AsyncStorage.removeItem('userToken');
-			// 		firebase.auth().signOut();
-			// 		this.props.navigation.navigate('Auth');
-			// 	}}
-			// />
+
+	initalizeGame = async () => {
+		const userToken = await AsyncStorage.getItem('userToken').then(
+			result => result
 		);
+		await firebase
+			.database()
+			.ref('users/' + userToken)
+			.once('value')
+			.then(snapshot => {
+				let user = snapshot.val();
+				this.setState({ user });
+				firebase
+					.database()
+					.ref('games/' + user.game)
+					.once('value')
+					.then(gameSnapshot => {
+						let game = gameSnapshot.val();
+						this.setState({ game: game }, () => this.getPlayerInfo(game));
+					})
+					.catch(err => console.log(err));
+			});
+	};
+
+	getPlayerInfo = (game: any): any => {
+		game.players.forEach((playerId: any) => {
+			firebase
+				.database()
+				.ref('users/' + playerId)
+				.once('value')
+				.then(snapshot => {
+					let player = snapshot.val();
+					this.setState({ players: [...this.state.players, player] });
+				});
+		});
+	};
+
+	renderPlayer = () => {
+		return this.state.players.map((player: any, key: number) => {
+			return (
+				<View key={key} style={{ alignItems: 'center' }}>
+					<Image
+						source={{ uri: player.profilePicture }}
+						style={{ height: 120, width: 120, borderRadius: 60 }}
+					/>
+					<Text>
+						{player.firstName} {player.lastName}
+					</Text>
+				</View>
+			);
+		});
+	};
+
+	render() {
+		if (this.state.game && this.state.players && this.state.user) {
+			return (
+				<View style={styles.container}>
+					<View>
+						<Text style={styles.gameNameText}>{this.state.game.gameName}</Text>
+					</View>
+					<View style={styles.profilePictureContainer}>
+						{this.renderPlayer()}
+					</View>
+				</View>
+			);
+		} else {
+			return (
+				<View style={styles.container}>
+					<ActivityIndicator />
+				</View>
+			);
+		}
 	}
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+		backgroundColor: '#fff',
+		padding: 20,
+		alignItems: 'center'
+	},
+	gameNameText: {
+		fontSize: 27
+	},
+	profilePictureContainer: {
+		flex: 0.5,
+		width: '100%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		paddingHorizontal: 20
 	}
 });
