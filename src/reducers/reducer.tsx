bb91a4bd652194ad3f9c8dc6a2dd5ec4b27e086d
Mousecop <joaskew13@gmail.com>
2018-07-23 12:48:23 -0400

    Redux reducer file

---
+++ src/reducers/reducer.tsx
@@ -0,0 +1,56 @@
+import * as actions from '../actions/actionTypes';
+import * as types from '../types/type';
+export interface BaseState {
+	currentUser: types.User;
+	playerTwo: types.User;
+	game: types.Game;
+}
+
+const initalState: BaseState = {
+	currentUser: {
+		email: '',
+		firstName: '',
+		game: '',
+		lastName: '',
+		points: 0,
+		profilePicture: '',
+		playerId: ''
+	},
+	playerTwo: {
+		email: '',
+		firstName: '',
+		game: '',
+		lastName: '',
+		points: 0,
+		profilePicture: '',
+		playerId: ''
+	},
+	game: {
+		gameName: '',
+		players: [],
+		points: [],
+		rules: [],
+		gameId: ''
+	}
+};
+
+export const reducer = (state = initalState, action: any) => {
+	switch (action.type) {
+		case actions.GET_CURRENT_USER_SUCCESS:
+			console.log('ACTION CURRENT USER:', action.user);
+			return { ...state, currentUser: action.user };
+		case actions.GET_PLAYER_TWO_SUCESS:
+			console.log('ACTION PLAYER TWO:', action.playerTwo);
+			return { ...state, playerTwo: action.playerTwo };
+		case actions.GET_GAME:
+			console.log('ACTION GAME:', action.game);
+			return { ...state, game: action.game };
+		case actions.SUBMIT_POINT:
+			return {
+				...state,
+				playerTwo: { ...state.playerTwo, points: state.playerTwo.points + 1 }
+			};
+		default:
+			return state;
+	}
+};
