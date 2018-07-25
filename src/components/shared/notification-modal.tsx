342e86aa8f3e6944e8e450df8fb2f184fb6c6049
Mousecop <joaskew13@gmail.com>
2018-07-25 12:30:30 -0400

    Adds conditional logic for submit point and cash in notification

--- src/components/shared/notification-modal.tsx
+++ src/components/shared/notification-modal.tsx
@@ -1,6 +1,6 @@
 import Modal from 'react-native-modal';
 import { View, Text, Dimensions } from 'react-native';
-import React from 'react';
+import React, { Component } from 'react';
 import { User } from '../../types/type';
 
 // Get dimensions
@@ -12,47 +12,103 @@ export interface Props {
 	playerTwo: User;
 	isVisible: boolean;
 	closeModal: any;
+	currentUser: User;
+	notificationMode: string;
 }
 
-export const NotificationModal = (props: Props) => {
-	let { reason, playerTwo, isVisible, closeModal } = props;
-	let modalVisible = isVisible;
-	console.log('MODAL VISIBLE:', modalVisible);
-	return (
-		<View>
-			<Modal
-				isVisible={modalVisible}
-				onSwipe={() => {
-					closeModal();
-				}}
-				swipeDirection="down"
-				onBackdropPress={() => {
-					closeModal();
-				}}
-				style={{
-					justifyContent: 'flex-start',
-					marginTop: 15,
-					alignItems: 'center'
-				}}
-				animationOutTiming={500}
-				backdropOpacity={0.5}
-				animationIn="slideInDown">
-				<View
-					style={{
-						backgroundColor: '#fff',
-						height: deviceHeight / 9.5,
-						width: deviceWidth / 1.1,
-						padding: 15,
-						borderRadius: 10,
-						justifyContent: 'space-evenly'
-					}}>
-					<Text style={{ fontSize: 16, paddingBottom: 10 }}>
-						You have received a point from {playerTwo.firstName}{' '}
-						{playerTwo.lastName}!
-					</Text>
-					<Text style={{ fontSize: 14 }}>For: {reason.trim()}</Text>
+export class NotificationModal extends Component<Props> {
+	constructor(props: Props) {
+		super(props);
+	}
+	render() {
+		let {
+			notificationMode,
+			currentUser,
+			isVisible,
+			closeModal,
+			playerTwo,
+			reason
+		} = this.props;
+		let modalVisible = isVisible;
+		console.log('notificationMode:', notificationMode);
+		if (notificationMode === 'submitPoint') {
+			return (
+				<View>
+					<Modal
+						isVisible={modalVisible}
+						onSwipe={() => {
+							closeModal();
+						}}
+						swipeDirection="down"
+						onBackdropPress={() => {
+							closeModal();
+						}}
+						style={{
+							justifyContent: 'flex-start',
+							marginTop: 15,
+							alignItems: 'center'
+						}}
+						animationOutTiming={500}
+						backdropOpacity={0.5}
+						animationIn="slideInDown">
+						<View
+							style={{
+								backgroundColor: '#fff',
+								height: deviceHeight / 9.5,
+								width: deviceWidth / 1.1,
+								padding: 15,
+								borderRadius: 10,
+								justifyContent: 'space-evenly'
+							}}>
+							<Text style={{ fontSize: 16, paddingBottom: 10 }}>
+								You have received a point from {playerTwo.firstName}{' '}
+								{playerTwo.lastName}!
+							</Text>
+							<Text style={{ fontSize: 14 }}>For: {reason.trim()}</Text>
+						</View>
+					</Modal>
 				</View>
-			</Modal>
-		</View>
-	);
-};
+			);
+		} else if (notificationMode === 'cashIn') {
+			return (
+				<View>
+					<Modal
+						isVisible={modalVisible}
+						onSwipe={() => {
+							closeModal();
+						}}
+						swipeDirection="down"
+						onBackdropPress={() => {
+							closeModal();
+						}}
+						style={{
+							justifyContent: 'flex-start',
+							marginTop: 15,
+							alignItems: 'center'
+						}}
+						animationOutTiming={500}
+						backdropOpacity={0.5}
+						animationIn="slideInDown">
+						<View
+							style={{
+								backgroundColor: '#fff',
+								height: deviceHeight / 9.5,
+								width: deviceWidth / 1.1,
+								padding: 15,
+								borderRadius: 10,
+								justifyContent: 'space-evenly'
+							}}>
+							<Text style={{ fontSize: 16, paddingBottom: 10 }}>
+								{currentUser.firstName} just cashed in {currentUser.points}{' '}
+								point{currentUser.points > 1 ? 's' : ''}!
+							</Text>
+							<Text style={{ fontSize: 14 }}>For: {reason.trim()}</Text>
+						</View>
+					</Modal>
+				</View>
+			);
+		} else {
+			return '';
+		}
+	}
+}
